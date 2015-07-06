21234df9f5bcdc294e7a8cf8b4cd222ab5106f46
Roelof Pieters <graphific@gmail.com>
2015-07-06 17:42:55 +0200

    script for creating animated gifs

---
+++ frames2gif.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+if [ $# -eq 0 ]; then
+    echo "please provide the moviename and directory where to store the frames"
+    echo "./frames2gif.sh [directory] [frames_a_second] [filename.gif]"
+    exit 1
+fi
+
+ffmpeg -f image2 -framerate $2 -i $1/%04d.jpg $3
