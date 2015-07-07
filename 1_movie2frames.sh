5064603666fe015a47b8ca7fb61e574198ba39c4
Roelof Pieters <graphific@gmail.com>
2015-07-07 18:09:00 +0200

    choose between avconf || ffmpeg
    option to choose between avconf or ffmpeg (from macpod)

--- 1_movie2frames.sh
+++ 1_movie2frames.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
     echo "please provide the moviename and directory where to store the frames"
-    echo "./1_movie2frames [movie.mp4] [directory]"
+    echo "./1_movie2frames [ffmpeg|avconv] [movie.mp4] [directory]"
     exit 1
 fi
 
-mkdir -p $2
-ffmpeg -i $1 -r 25.0 $2/%4d.jpg
+mkdir -p $3
+if [ "avconf" == "$1" ]; then
+    avconv -i $2 -vsync 1 -r 25 -an -y -qscale 0 $3/%04d.jpg
+else
+    ffmpeg -i $2 -r 25.0 $3/%4d.jpg
+fi
