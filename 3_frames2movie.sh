d428231ff79c939d124f5a72d441b3d4d7281c5b
Roelof Pieters <graphific@gmail.com>
2015-07-08 00:22:45 +0200

    Update 3_frames2movie.sh

--- 3_frames2movie.sh
+++ 3_frames2movie.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
+if [ $# -ne 2 ]; then
     echo "please provide the directory of the processed frames"
-    echo "./2_frames2movie [directory]"
+    echo "./2_frames2movie [frames_directory] [original_video_with_sound]"
     exit 1
 fi
 
 ffmpeg -framerate 25 -i $1/%4d.jpg -c:v libx264 -r 30 -pix_fmt yuv420p tmp.mp4
 
-ffmpeg -i original.mp4 original.mp3
+ffmpeg -i $2 original.mp3
 ffmpeg -i original.mp3 music.wav
 
 secs=$(ffprobe -i $1.mp4 -show_entries format=duration -v quiet -of csv="p=0")
