d073dbddd74af1d0b64c6e87cfb4bc1d5da25f2c
CoreyTrombley <corey.trombley33@gmail.com>
2015-07-07 22:34:16 -0400

    Fixed the secs calculation, added some output about deleting files

--- 3_frames2movie.sh
+++ 3_frames2movie.sh
@@ -10,7 +10,16 @@ ffmpeg -framerate 25 -i $1/%4d.jpg -c:v libx264 -r 30 -pix_fmt yuv420p tmp.mp4
 ffmpeg -i $2 original.mp3
 ffmpeg -i original.mp3 music.wav
 
-secs=$(ffprobe -i $2.mp4 -show_entries format=duration -v quiet -of csv="p=0")
+secs=$(ffprobe -i tmp.mp4 -show_entries format=duration -v quiet -of csv="p=0")
 ffmpeg -i music.wav -ss 0 -t $secs musicshort.wav
 ffmpeg -i musicshort.wav -i tmp.mp4 -strict -2 $1.mp4
+
+echo 'Removing temp files'
+rm original.mp3
+echo "original.mp3 removed"
+rm music.wav
+echo "music.wav removed"
+rm musicshort.wav
+echo "musicshort.wav removed"
 rm tmp.mp4
+echo "tmp.mp4 removed"
