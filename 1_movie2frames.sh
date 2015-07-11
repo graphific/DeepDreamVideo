05715ad832a4a71fe6d5cfe6c6114002033c7c4a
Denjell <denjell@das-gaengeviertel.info>
2015-07-11 16:22:47 +0200

    EVERYWHERE - for future proofing now using %08d instead of %04d - added double quotes around user entered strings before they are parsed by CLI programs - using which to figure out where to call the program from, eg `/usr/bin/mplayer` instead of `mplayer`
    1_movie2frames.sh
    - autodetection of fps
    - added mplayer option
    
    3_frames2movie.sh
    - output mp4 is now of better quality
    
    frames2gif.sh
    - added graphicsmagick / imagemagick options for gif creation
    - detect FPS instead of "know" it

--- 1_movie2frames.sh
+++ 1_movie2frames.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 if [ $# -ne 3 ]; then
     echo "please provide the moviename and directory where to store the frames"
-    echo "./1_movie2frames [ffmpeg|avconv] [movie.mp4] [directory]"
+    echo "./1_movie2frames [ffmpeg|avconv|mplayer] [movie.mp4] [directory]"
     exit 1
 fi
 
-mkdir -p $3
+mkdir -p "$3"
+rm -R "$3/*"
 if [ "avconv" == "$1" ]; then
-    avconv -i $2 -vsync 1 -r 25 -an -y -qscale 0 $3/%04d.jpg
+    AVCONV=$(which avconv)
+    FPS=$($AVCONV -i filename 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p") # this line is not tested cuz i don't have avconv :(
+    $AVCONV -i "$2" -vsync 1 -r ${FPS} -an -y -qscale 0 "$3/%08d.jpg"
+elif [ "mplayer" == "$1" ]; then
+    MPLAYER=$(which mplayer)
+    # mplayer automatically converts video to images at one image per frame (so no need for the FPS), so the following line is not necessary - but for the record, this is what you would use:
+    # FPS=$($MPLAYER -really-quiet -vo null -ao null -frames 0 -identify "${2}" | grep 'ID_VIDEO_FPS' | cut -d'=' -f2)
+    $MPLAYER -vo "jpeg:outdir=$3" -ao null "$2"
 else
-    ffmpeg -i $2 -r 25.0 $3/%4d.jpg
+    FFMPEG=$(which ffmpeg)
+    FFPROBE=$(which ffprobe)
+    FPS=$($FFPROBE -show_streams -select_streams v -i "$2"  2>/dev/null | grep "r_frame_rate" | cut -d'=' -f2)
+    $FFMPEG -i "$2" -r ${FPS} -f image2 "$3/%08d.jpg"
 fi
