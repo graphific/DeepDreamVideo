1539df706051f8326b575d0f057dd1602f160b6b
Jason Sorensen <git@smert.net>
2015-07-15 10:36:59 +0000

    Add mplayer support for stage 3.

--- 3_frames2movie.sh
+++ 3_frames2movie.sh
@@ -7,8 +7,10 @@ fi
 
 if [ "png" == "$4" ]; then
     INFILES="$2/%08d.png"
+    MENCODERCOMMAND="type=png"
 else
     INFILES="$2/%08d.jpg"
+    MENCODERCOMMAND="type=jpg"
 fi
 
 CODEC="libx264"
@@ -24,6 +26,7 @@ if [ "avconv" == "$1" ]; then
     AVCONV=$(which avconv)
     BITRATE=$($AVCONV -i "$3" 2>&1 | sed -n "s/.*, \([0-9].*\) kb\/s.*/\1/p")
     FPS=$($AVCONV -i "$3" 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p")
+
     ${AVCONV} -r ${FPS} -i "${INFILES}" -b:v "${BITRATE}k" -c:v ${CODEC} -vf "format=yuv420p" "${TMPVIDEO}" -y
     ${AVCONV} -i "$3" -strict -2 "${TMPAUDIO}" -y
 
@@ -33,12 +36,16 @@ if [ "avconv" == "$1" ]; then
         ${AVCONV}                  -i "${TMPVIDEO}" -strict -2 -c:v copy -shortest "${OUTFILE}"
     fi
 elif [ "mplayer" == "$1" ]; then
-    echo "Mplayer not supported yet."
-    exit 1
+    MENCODER=$(which mencoder)
+    MPLAYER=$(which mplayer)
+    BITRATE=$(( $($MPLAYER -really-quiet -vo null -ao null -frames 0 -identify "${3}" | grep 'ID_VIDEO_BITRATE' | cut -d'=' -f2) / 1000 ))
+    FPS=$($MPLAYER -really-quiet -vo null -ao null -frames 0 -identify "${3}" | grep 'ID_VIDEO_FPS' | cut -d'=' -f2)
+
+    ${MENCODER} "mf://${INFILES}" -mf "fps=${FPS}:${MENCODERCOMMAND}" -oac copy -ovc x264 -x264encopts bitrate=${BITRATE} -ofps ${FPS} -o "${OUTFILE}"
+    # Missing audio
 else
     FFMPEG=$(which ffmpeg)
     FFPROBE=$(which ffprobe)
-
     FPS=$(${FFPROBE} -show_streams -select_streams v -i "$3" 2>/dev/null | grep "r_frame_rate" | cut -d'=' -f2)
 
     ${FFMPEG} -framerate ${FPS} -i "${INFILES}" -c:v ${CODEC} -vf "fps=${FPS},format=yuv420p" -tune fastdecode -tune zerolatency -profile:v baseline "${TMPVIDEO}" -y
@@ -61,6 +68,6 @@ rm "${TMPAUDIO}"
 rm "${TMPVIDEO}"
 #echo "${TMPVIDEO} removed"
 
-if [ -f ${OUTFILE} ]; then
+if [ -s ${OUTFILE} ]; then
     echo "saved movie as: ${OUTFILE}"
 fi
